8a0ba6a832af77dfd9d006437eec4dfc7291aaab
Jordan Bowman <hello@jrdnbwmn.com>
2014-01-15 15:03:48 -0800

    Small fixes/improvements

--- gruntfile.js
+++ gruntfile.js
@@ -84,7 +84,7 @@ module.exports = function(grunt){
                     expand: true,
                     cwd: 'images/',
                     src: ['**/*.{png,jpg,gif}'],
-                    dest: '/build/images'
+                    dest: 'build/images'
                 }]
             }
         },
